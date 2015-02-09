2f3a5f285b20e50644c725b192a9e882b41a7ef2
Luke Winslow <lwinslow@usgs.gov>
2015-02-09 14:41:24 -0600

    UUID code added

--- iplover-ui/src/main/webapp/js/plugins.js
+++ iplover-ui/src/main/webapp/js/plugins.js
@@ -1,4 +1,17 @@
 
+// Create UUID used to uniquely identify plover records
+// From http://stackoverflow.com/questions/105034/create-guid-uuid-in-javascript
+generateUUID = function(){
+    var d = new Date().getTime();
+    var uuid = 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = (d + Math.random()*16)%16 | 0;
+        d = Math.floor(d/16);
+        return (c=='x' ? r : (r&0x3|0x8)).toString(16);
+    });
+    return uuid;
+};
+
+
 // usage: log('inside coolFunc', this, arguments);
 // paulirish.com/2009/log-a-lightweight-wrapper-for-consolelog/
 window.log = function(){
