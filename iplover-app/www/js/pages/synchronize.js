00e9ef29c7d320c69c80ce380b26ea6f31113bb3
Luke Winslow <lwinslow@usgs.gov>
2015-03-04 17:27:40 -0600

    Added error message on sync error

--- iplover-app/www/js/pages/synchronize.js
+++ iplover-app/www/js/pages/synchronize.js
@@ -29,7 +29,7 @@ var onfinish  = function(){
 var onerror = function(error){
     $('.percent').html('0%');
     $('.bar').width('0%');
-    
+    alert("Error occurred. Please restart iPlover and try again.\nContact mhines@usgs.gov if issues persist");
 };
 
 
