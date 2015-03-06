d62644cc7111e4533a35a236774a3145fcf5c779
Luke Winslow <lwinslow@usgs.gov>
2015-03-06 14:13:23 -0600

    final touches

--- iplover-app/www/js/config.js
+++ iplover-app/www/js/config.js
@@ -2,6 +2,6 @@ if(typeof iplover === 'undefined'){
 	iplover = [];
 }
 
-iplover.recordsurl = "http://cidasdqaasiplvr.cr.usgs.gov:8080/iplover/service/v2/records";
-iplover.photourl   = "http://cidasdqaasiplvr.cr.usgs.gov:8080/iplover/service/v2/photos";
+iplover.recordsurl = "https://cida-test.er.usgs.gov/iploverqa/service/v2/records";
+iplover.photourl   = "https://cida-test.er.usgs.gov/iplover/service/v2/photos";
 
