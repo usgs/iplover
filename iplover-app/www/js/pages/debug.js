2123277a4de3aff173fb16ad1bcce509b050edc8
Luke Winslow <lwinslow@usgs.gov>
2015-06-01 17:06:24 -0500

    Change for better debug log comm

---
+++ iplover-app/www/js/pages/debug.js
@@ -0,0 +1,17 @@
+
+$(document).ready(function() {
+    
+
+    	records_json = JSON.stringify(iplover.data.getChangedRecords());
+    	nonserver_json = JSON.stringify(iplover.data.getNonserverRecords());
+    	all_rec = JSON.stringify(iplover.data.getRecords());
+    	
+    	debug_text = "User Token:" + iplover.auth.getToken() + "\n" +
+    				"User Group:" + iplover.data.getGroup() + "\n" + 
+    				"Nonserver:" + nonserver_json + "\n" +
+    				"Changed:" + records_json + "\n" + 
+    				"All:" + all_rec;
+    	
+    	$("#debugtext").val(debug_text);
+    
+});
